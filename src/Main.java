b07306a65af2a0b6f06cf18e431438dff47fbbfa
Clare <j.clare@campus.fct.unl.pt>
2026-09-19 17:29:48 +0100

    2 tests working GN command implemented

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_CONTACT    = "GN";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -53,6 +54,9 @@ public class Main {
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
                     break;
+                case GET_CONTACT:
+                    getContact(in, cBook);
+                    break;
                 default:
                     System.out.println(COMMAND_ERROR);
             }
@@ -83,6 +87,13 @@ public class Main {
             System.out.println(CONTACT_ADDED);
         }
         else System.out.println(CONTACT_EXISTS);
+    }
+
+    private static void getContact(Scanner in, ContactBook cBook) {
+        int phone = in.nextInt(); in.nextLine();
+        if (cBook.hasContact(phone))
+            System.out.println(cBook.getName(phone));
+        else System.out.println("Phone number does not exist.");//TODO fazer a constante
     }
 
     private static void deleteContact(Scanner in, ContactBook cBook) {
--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -1,7 +1,5 @@
 package contactBook;
 
-import contactBook.Contact;
-
 public class ContactBook {
     static final int DEFAULT_SIZE = 100;
 
@@ -18,6 +16,11 @@ public class ContactBook {
     //Pre: name != null
     public boolean hasContact(String name) {
         return searchIndex(name) >= 0;
+    }
+
+    //Pre: phone != null
+    public boolean hasContact(int phone) {
+        return searchIndex(phone) >= 0;
     }
 
     public int getNumberOfContacts() {
@@ -50,6 +53,11 @@ public class ContactBook {
         return contacts[searchIndex(name)].getEmail();
     }
 
+    //Pre: phone != null && hasContact(phone)
+    public String getName(int phone) {
+        return contacts[searchIndex(phone)].getName();
+    }
+
     //Pre: name != null && hasContact(name)
     public void setPhone(String name, int phone) {
         contacts[searchIndex(name)].setPhone(phone);
@@ -70,6 +78,21 @@ public class ContactBook {
             else
                 i++;
         if (found) result = i;
+        return result;
+    }
+
+    private int searchIndex(int phone) {
+        int i = 0;
+        int result = -1;
+        boolean found = false;
+        while (i < counter && !found) {
+            if (contacts[i].getPhone() == phone)
+                found = true;
+            else
+                i++;
+        }
+        if (found)
+            result = i;
         return result;
     }
 
